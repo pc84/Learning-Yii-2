db3986c70056b7d9e3d1ab46edbf2cd6243876d6
pc84 <pedroecastineiras@gmail.com>
2015-12-28 05:13:25 -0500

    updating database credentials

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=callapp',
+    'username' => 'callappuser',
+    'password' => 'MMTzCUrqXFmCAzuQ',
     'charset' => 'utf8',
 ];
